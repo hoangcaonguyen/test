3a04517d7c5324657e8b1f5554b4a2226223217a
nguyen hoang <overlord.x.king@gmail.com>
2022-02-24 21:36:10 +0700

    abc

---
+++ test.js
@@ -0,0 +1,11 @@
+let n = prompt("nhập số n: ");
+let i = prompt("nhập inputNumber: ");
+
+
+function findOppositeNumber(n, i) {
+    if (n <= 4 || n >= 20) {
+        console.log("n phải nằm trong khoảng từ 4 đến 20");
+    }
+}
+
+findOppositeNumber(n, i);
